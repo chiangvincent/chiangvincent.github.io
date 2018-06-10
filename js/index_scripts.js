72b40d7c14f23dfed83ce4134f2ffc42665252a9
Vincent Chiang <vincentchiang@berkeley.edu>
2018-06-09 22:39:16 -0700

    finished framework need info

---
+++ js/index_scripts.js
@@ -0,0 +1,42 @@
+//function displayProject() {
+//    var text = document.getElementById("projects"); 
+//    text.style.display = "block"; 
+//    var placeholder = document.getElementById("placeholder"); 
+//    placeholder.style.display = "none"; 
+//}
+// 
+
+//$(document).ready(function() {
+//    $("#project_button").click(function() {
+//        $("#projects").fadeIn("medium");
+//        $("#placeholder").hide();
+//    });
+//}); 
+
+
+$(document).ready(function() {
+    $(".buttons button").click(function(){
+        $(this).displayInfo(this);
+     });
+     $.fn.displayInfo = function(button) {
+        console.log("hi"); 
+        var displayed = ["projects", "music", "placeholder", "experience"]; 
+        for (i=0;i<displayed.length; i++) {
+            string = displayed[i]; 
+            console.log(string); 
+            if (string == button.id) {
+                var toShow = string += "_text"; 
+                console.log("#" + toShow); 
+                $("#" + toShow).fadeIn("medium");
+            } else {
+                if (string == "placeholder") {
+                    $("#" + string).hide(); 
+                } else {
+                    var toShow = string += "_text"; 
+                    $("#" + toShow).hide(); 
+                }
+            }
+        }
+        
+     }
+});
